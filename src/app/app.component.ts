4286f9438355d303666535269e6febcffa881657
FredericoTotvs <frederico.rodrigues@totvs.com.br>
2024-06-21 19:40:09 -0300

    add all project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherCardComponent } from '../components/weather-card/weather-card.component';
+import  {HttpClientModule }  from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WeatherCardComponent, HttpClientModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
